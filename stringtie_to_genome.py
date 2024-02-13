4cca54cbc9b7f9690649bac676e3175baa075d0c
Michael Gribskov <mgribsko@gmail.com>
2024-02-13 14:29:11 -0500

    stringtie_to_genome.py - work in progress, does not work

--- stringtie_to_genome.py
+++ stringtie_to_genome.py
@@ -4,7 +4,7 @@ map stringtie transcripts to genome GFF file
 Michael Gribskov     13 February 2024
 ================================================================================================="""
 import sys
-from gff import Gff
+from gff import Gff, Dotdict
 
 
 def seq_begin_sorter(data):
@@ -16,9 +16,28 @@ def seq_begin_sorter(data):
     ---------------------------------------------------------------------------------------------"""
     data = data.data
     for entry in sorted(data, key=lambda l: (l['sequence'], l['begin'])):
-        yield entry
+        yield Dotdict(entry)
 
     return
+
+def advance_sequence(ggen, sgen):
+    """---------------------------------------------------------------------------------------------
+    advance the pointer that is behind until both are on the chromosome. Assumes that g and s are
+    seq_begin_sorter generators
+
+    :param gptr: seq_begin_sorter (genome)
+    :param sptr: seq_begin_sorter (stringtie)
+    :return: bool, True if sequence records match, false otherwise
+    ---------------------------------------------------------------------------------------------"""
+    while g['sequence'] < s['sequence']:
+        g = next(ggen)
+    while g['sequence'] > s['sequence']:
+        s = next(sgen)
+
+    return True
+
+
+
 
 
 # --------------------------------------------------------------------------------------------------
@@ -38,7 +57,41 @@ if __name__ == '__main__':
     sys.stderr.write(f'{n_transcripts} transcripts read from {stringtie_file}\n')
 
     s_order = seq_begin_sorter(transcripts)
-    for entry in s_order:
-        print(f"{entry['sequence']}\t{entry['begin']}\t{entry['end']}")
+    # for entry in s_order:
+    #     print(f"{entry.sequence}\t{entry.begin}\t{entry.end}")
+
+    g_order = seq_begin_sorter(genome)
+    # for entry in g_order:
+    #     print(f"{entry.sequence}\t{entry.begin}\t{entry.end}")
+
+    # for each stringtie transcript, advance the genome pointer until the end coordinate is past the stringtie-begin
+    # coordinate
+
+    s = next(s_order)
+    g = next(g_order)
+    while True:
+        if g.sequence < s.sequence:
+            print(f"    gs   {s.sequence}\t{s.begin}\t{s.end}\t\t{g.sequence}\t{g.begin}\t{g.end}")
+            g = next(g_order)
+            continue
+        elif g.sequence > s.sequence:
+            print(f"    ss   {s.sequence}\t{s.begin}\t{s.end}\t\t{g.sequence}\t{g.begin}\t{g.end}")
+            s = next(s_order)
+            continue
+
+        # if you reach here its g and s are on the same sequence
+
+        if g.begin > s.end:
+            print(f"    sa   {s.sequence}\t{s.begin}\t{s.end}\t\t{g.sequence}\t{g.begin}\t{g.end}")
+            s = next(s_order)
+            continue
+        elif g.end < s.begin:
+            print(f"    ga   {s.sequence}\t{s.begin}\t{s.end}\t\t{g.sequence}\t{g.begin}\t{g.end}")
+            g = next(g_order)
+            continue
+        else:
+            print(f"ovverlap {s.sequence}\t{s.begin}\t{s.end}\t\t{g.sequence}\t{g.begin}\t{g.end}")
+
+
 
     exit(0)
